d86ecf1529356e41f003d9185a3993c1d23a41d3
Chris Brighton <christopher.brighton@netnatives.co.uk>
2024-01-24 15:11:19 +0000

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  },
   build: {
     transpile: ['vuetify'],
   },
